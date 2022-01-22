036f48bcbd5f8af3e2f66f874b407578fe106919
mbutle3 <Growth_144$>
2022-01-22 11:39:13 -0500

    v1.5 - currnet bug (new posts are not being shown)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(const MyApp());
 }
 
+class Post {
+  String body;
+  String author;
+  int like_count = 0;
+  bool userLiked = false;
+
+  Post(this.body, this.author);
+
+  void likedPost() {
+    this.userLiked = !this.userLiked;
+    if (this.userLiked) {
+      this.like_count += 1;
+    } else {
+      this.like_count -= 1;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -27,11 +45,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class __MyHomePageState extends State<MyHomePage> {
-  String text = "";
+  List<Post> posts = [];
 
-  void changeText(String text) {
+  void newPosts(String text) {
     this.setState(() {
-      this.text = text;
+      posts.add(new Post(text, "Author"));
     });
   }
 
@@ -40,8 +58,8 @@ class __MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(title: Text("Chat Lobby App")),
         body: Column(children: <Widget>[
-          TextInputWidget(this.changeText),
-          Text(this.text)
+          Expanded(child: PostList(this.posts)),
+          Expanded(child: Expanded(child: TextInputWidget(this.newPosts)))
         ]));
   }
 }
@@ -84,5 +102,40 @@ class _TextInputWidgetState extends State<TextInputWidget> {
                 tooltip: "sends a message to chatbox",
               )))
     ]);
+  }
+}
+
+class PostList extends StatefulWidget {
+  final List<Post> list_items;
+
+  PostList(this.list_items);
+
+  @override
+  _PostListState createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.widget.list_items.length,
+      itemBuilder: (context, index) {
+        var post = this.widget.list_items[index];
+        return Card(
+            child: Row(children: <Widget>[
+          Expanded(
+              child: ListTile(
+            title: Text(post.body),
+            subtitle: Text(post.author),
+          )),
+          Row(children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.thumb_up),
+              onPressed: post.likedPost,
+            )
+          ])
+        ]));
+      },
+    );
   }
 }
