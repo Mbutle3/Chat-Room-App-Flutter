52e83f708a3231178e6f246e670320089be535ef
mbutle3 <Growth_144$>
2022-01-23 22:03:35 -0500

    cleared login page before adding google auth

--- lib/login.dart
+++ lib/login.dart
@@ -33,20 +33,6 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     return Align(
         alignment: Alignment.center,
-        child: Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-                controller: this.controller,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 5, color: Colors.black)),
-                  labelText: "Enter Username:",
-                  suffixIcon: IconButton(
-                      icon: Icon(Icons.check_box),
-                      splashColor: Colors.blue,
-                      tooltip: "sends a message to chatbox",
-                      onPressed: this.click),
-                ))));
+    )
   }
 }
